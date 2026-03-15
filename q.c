136c37f9df4a97ae083d6a6dc1d833d17a7ebb13
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-03-15 15:54:03 +0300

    selection logic partially done

--- q.c
+++ q.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-double gamma; // curiosity coefficient
-double epsilon; // epsilon-greedy thing exploration vs exploitation
-double alpha; // learning coefficient
+const double gamma = 0.95; // curiosity coefficient
+double epsilon = 0.8; // epsilon-greedy thing exploration vs exploitation
+double omEpsilon = 0.2; // One Minus Epsilon
+const double alpha = 0.1; // learning coefficient
 
 typedef enum {LEFT, RIGHT, UP, DOWN, LOAD, UNLOAD} Action;
 
@@ -35,6 +36,7 @@ int main(){
 	int minTrials = 1000;
 	int trialCount = 0;
 	int bool_converged = 0;
+	// !!!!!! call stateupdater thing with truck1
 //	for(; trialCount <= minTrials && !bool_converged; trialCount++){
 //		while(){ // continue until loaded successfully
 //			first take an action
@@ -49,34 +51,56 @@ int main(){
 double judge(int row, int col, int loaded){
 	return 0; // dummy
 }
-
-double *landNext(int row, int col, Action action){
+/*returns the address for where the movement based actions lead*/
+double *landNext(int row, int col, int loaded, Action action){
 	if(action == LEFT)
-		return &state[row * 10 + col - 1];
+		return &state[row * 10 + col - 1][loaded][LEFT];
 	else if(action == RIGHT)
-		return &state[row * 10 + col + 1];
+		return &state[row * 10 + col + 1][loaded][RIGHT];
 	else if(action == UP)
-		return &state[(row + 1) * 10 + col];
+		return &state[(row + 1) * 10 + col][loaded][UP];
 	else if(action == DOWN){
-		return &state[(row - 1) * 10 + col];
+		return &state[(row - 1) * 10 + col][loaded][DOWN];
 	}
 	else
 		return NULL;
 	// TODO handle out of bounds!!
 }
 
-void updateParticularState(AgentPtr truck, Action action){ // technically we update where the agent is at don't we?
+/*checks the coordinates and returns bool whether we are at load or no*/
+int atLoadPosition(){
+	return 0; // dummy
+}
+
+int atUnloadPosition(){
+	return 0; // dummy
+}
+
+void updateParticularState(AgentPtr truck, Action action){
 	int row = truck->locRow;
 	int col = truck->locCol;
 	int loaded = truck->loaded;
 	double reward = judge(row, col, loaded);
-	double *landing = NULL;
-
 	double *cur = &state[row * 10 + col][loaded][action];
-	if(action != LOAD && action != UNLOAD)
-		landing = landNext(row, col, action);
-	*cur = *cur + alpha * (reward + gamma * landing[loaded][action]); // fix the syntax
+	double *landing = cur;
 
+	if(action != LOAD && action != UNLOAD) // moving
+		landing = landNext(row, col, loaded, action);
+	else if(action == LOAD && atLoadPosition() && loaded == 0){ // not loaded already
+		landing = &state[row * 10 + col][1][LOAD];
+		loaded = 1;
+		// if it were to be already loaded it doesn't matter for it to re-load
+	}
+	else if(action == UNLOAD && atUnloadPosition() && loaded == 1){ // winning move btw
+		loaded = 0;
+		landing = &state[row * 10 + col][0][UNLOAD];
+	}
+	else{ // invalid load & unload. doesn't matter doesn't count as steps even
+		// idk we gotta do something here?
+	}
+	*cur = *cur + alpha * (reward + gamma * *landing - *cur);
 
-	// calculate where that action lands us at and then also make sure to update the truck struct's positions
+	// make sure to update the truck struct's positions
+	truck->loaded = loaded;
+	// TODO row col, div mod 
 }
