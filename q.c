b722d1b2c1c16099ed00d7e6bf2fdba999e7730d
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-03-15 12:17:09 +0300

    enum & struct definition

--- q.c
+++ q.c
@@ -3,7 +3,21 @@
 double gamma; // curiosity coefficient
 double epsilon; // epsilon-greedy thing
 double alpha; // learning coefficient
+
+enum actions {LEFT, RIGHT, UP, DOWN, LOAD, UNLOAD};
+typedef enum actions Action;
+
+typedef struct{
+	int row;
+	int col;
+}State, *StatePtr;
+
+void updateParticularState(State s, Action a);
+
 int main(){
 
 }
 
+void updateParticularState(State s, Action a){
+
+}
