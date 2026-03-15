5dfb7817192982715af6be28692ffae63df2c527
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-03-15 12:47:11 +0300

    lookup table attempt

--- q.c
+++ q.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 double gamma; // curiosity coefficient
-double epsilon; // epsilon-greedy thing
+double epsilon; // epsilon-greedy thing exploration vs exploitation
 double alpha; // learning coefficient
 
-enum actions {LEFT, RIGHT, UP, DOWN, LOAD, UNLOAD};
-typedef enum actions Action;
+typedef enum {LEFT, RIGHT, UP, DOWN, LOAD, UNLOAD} Action;
 
-typedef struct{
-	int row;
-	int col;
-}State, *StatePtr;
+typedef struct{ // current state of the truck
+	short LocRow;
+	short LocCol;
+	short loaded;
+}Agent, *AgentPtr;
 
-void updateParticularState(State s, Action a);
+double state[100][2][6] = 0.0; // 10x10 map, loaded vs unloaded (2), 6 actions
+
+void updateParticularState();
 
 int main(){
+	// stop when converges.
+	int minTrials = 1000;
+	int trialCount = 0;
+	int bool_converged = 0;
+//	for(; trialCount <= minTrials && !bool_converged; trialCount++){}
 
 }
 
-void updateParticularState(State s, Action a){
+void updateParticularState(){
 
 }
